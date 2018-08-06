cf8d831ab3c45e4818fb75c9749baf6eec26c17d
Dominik Volkamer <dominik.volkamer@fau.de>
2018-08-06 17:34:44 +0200

    updated date of chrismas closing for 2017 and rebuilt function

--- xmascc.py
+++ xmascc.py
@@ -5,15 +5,15 @@
 import datetime
 from datetime import date,timedelta
 
-#fromdate = date(2016,12,23)
+#fromdate = date(2017,11,7)
 now = datetime.datetime.now()
 
 fromdate = date(now.year, now.month, now.day)
-todate = date(2016,12,23)
+todate = date(2017,12,22)
 
 daygenerator = (fromdate + timedelta(x + 1) for x in range((todate - fromdate).days))
 
 countdown = sum(1 for day in daygenerator if day.weekday() < 5) + 1
 
-def print_countdown():
-  print("///// Nur noch %s Mal essen gehen bis zur Weihnachtsschließung der FAU. /////" %countdown)
+def get_countdown():
+  return "///// Nur noch %s Mal essen gehen bis zur Weihnachtsschließung der FAU. /////" %countdown
