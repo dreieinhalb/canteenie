1b755401d6415d46063bf094b002a5c5eebe8b86
MPi <markus.pinkert@fau.de>
2018-08-10 14:08:06 +0200

    sodele. Keine Ahnung, ob Python das kann

--- canteenie.py
+++ canteenie.py
@@ -4,10 +4,67 @@
 
 import argparse
 import datetime
-from openmensa import OpenMensa as openmensa
 import re
+from openmensa import OpenMensa as openmensa
 from termcolor import colored
 
+###############################################################################
+# map for assignment of IDs for FAU canteens
+fau_mensa = {
+    "lmpl" : 264,
+    "sued" : 256,
+    "isch" : 265,
+}
+
+# map for assignment of categories to abbreviations
+category_map = {
+    "Vegetarisch" : "V",
+    "Schwein"     : "S",
+    "Rind"        : "R",
+    "Geflügel"    : "G",
+    "Fisch"       : "F",
+}
+
+###############################################################################
+# command line arguments
+parser = argparse.ArgumentParser(description='A small python script that prints today\'s canteen/mensa menu for FAU on console.')
+parser.add_argument(
+        '-m', '--mensa',
+        help     = 'for which mensa? (lmpl: Erlangen Langemarckplatz (default), sued: Erlangen Süd, isch: Nürnberg Insel Schütt)',
+        choices  = ['lmpl', 'sued', 'isch'],
+        default  = "lmpl",
+        required = False
+)
+parser.add_argument(
+        '-i', '--id',
+        help     = 'for which ID on openmensa.org?',
+        default  = argparse.SUPPRESS, # don't define key if not given
+        required = False
+)
+parser.add_argument(
+        '-d', '--date',
+        help     = 'for which date (YYYY-MM-DD)?',
+        default  = argparse.SUPPRESS, # don't define key if not given
+        required = False,
+        type     = valid_date
+)
+parser.add_argument(
+        '-l', '--lite',
+        help     = 'disable ascii art header and color (lite view)',
+        action   = 'store_true',
+        default  = False,
+        required = False
+)
+parser.add_argument(
+        '-p', '--price',
+        help     = 'prices for which group? (employees (default), students, others)',
+        choices  = ['employees', 'students', 'others'],
+        default  = "employees",
+        required = False
+)
+args = vars(parser.parse_args())
+
+###############################################################################
 # function to validate date
 def valid_date(s):
     try:
@@ -19,114 +76,63 @@ def valid_date(s):
 # function to return colored text
 def colorize(text, color):
     # only return colored text if lite option is not set
-    if not args['lite']:
-        return colored(text, color)
-    else:
-        return text
+    if not args['lite']: return colored(text, color)
+    else:                 return text
 
 # function to return intentation
 def intent():
     # only return intentation if lite option is not set
-    if not args['lite']:
-        return "\t"
-    else:
-        return ''
+    if not args['lite']: return "\t"
+    else:                return ''
 
-# command line arguments
-parser = argparse.ArgumentParser(description='A small python script that prints today\'s canteen/mensa menu for FAU on console.')
-parser.add_argument(
-        '-m',
-        '--mensa',
-        help='for which mensa? (lmpl: Erlangen Langemarckplatz (default), sued: Erlangen Süd, isch: Nürnberg Insel Schütt)',
-        choices=['lmpl', 'sued', 'isch'],
-        default="lmpl",
-        required=False
-)
-parser.add_argument(
-        '-i',
-        '--id',
-        help='for which ID on openmensa.org?',
-        default=argparse.SUPPRESS, # don't define key if not given
-        required=False
-)
-parser.add_argument(
-        '-d',
-        '--date',
-        help='for which date (YYYY-MM-DD)?',
-        default=argparse.SUPPRESS, # don't define key if not given
-        required=False,
-        type=valid_date
-)
-parser.add_argument(
-        '-l',
-        '--lite',
-        help='disable ascii art header and color (lite view)',
-        action='store_true',
-        default=False,
-        required=False
-)
-parser.add_argument(
-        '-p',
-        '--price',
-        help='prices for which group? (employees (default), students, others)',
-        choices=['employees', 'students', 'others'],
-        default="employees",
-        required=False
-)
-args = vars(parser.parse_args())
-
-# map for assignment of IDs for FAU canteens
-fau_mensa = {"lmpl":264, "sued":256, "isch":265}
-
+###############################################################################
+# argument computing
 # use mensa Langemarckplatz as default, or use given ID from args
 openmensa_id = 264
-if 'mensa' in args:
-    openmensa_id = fau_mensa[args['mensa']]
-if 'id' in args:
-    openmensa_id = args['id']
-
+date         = datetime.datetime.now()
+if 'mensa' in args: openmensa_id = fau_mensa[args['mensa']]
+if 'id'    in args: openmensa_id =           args['id']
 # use today as default or given date from args
-date = datetime.datetime.now()
-if 'date' in args:
-    date = args['date']
+if 'date'  in args: date = args['date']
 
 # get meals from openmensa API
 meals = openmensa.get_meals_by_day(openmensa_id, date.strftime("%Y-%m-%d"))
 
+###############################################################################
 # print header (ascii art only in non lite version)
 if not args['lite']:
-    print(intent() + colorize("                             ",'yellow'))
-    print(intent() + colorize(" __  __                      ",'yellow'))
-    print(intent() + colorize("|  \/  | ___ _ __  ___  __ _ ",'yellow'))
-    print(intent() + colorize("| |\/| |/ _ \ '_ \/ __|/ _` |",'yellow'))
-    print(intent() + colorize("| |  | |  __/ | | \__ \ (_| |",'yellow'))
-    print(intent() + colorize("|_|  |_|\___|_| |_|___/\__,_|",'yellow'))
-    print(intent() + colorize("                             ",'yellow'))
+    iten = intent()
+    print(iten + colorize("                             ",'yellow'))
+    print(iten + colorize(" __  __                      ",'yellow'))
+    print(iten + colorize("|  \/  | ___ _ __  ___  __ _ ",'yellow'))
+    print(iten + colorize("| |\/| |/ _ \ '_ \/ __|/ _` |",'yellow'))
+    print(iten + colorize("| |  | |  __/ | | \__ \ (_| |",'yellow'))
+    print(iten + colorize("|_|  |_|\___|_| |_|___/\__,_|",'yellow'))
+    print(iten + colorize("                             ",'yellow'))
 print(intent() + colorize("//////// " + date.strftime("%d.%m.%Y") + " /////////", 'green'))
 print("")
 
-# map for assignment of categories to abbreviations
-category_map = {"Vegetarisch":"V", "Schwein":"S", "Rind":"R", "Geflügel":"G", "Fisch":"F"}
-
+###############################################################################
+# generating menu
 # set counter for numbering meals
 i = 1;
 for meal in meals:
     number = "(" + str(i) + ") "
+
     # remove duplicate and trailing spaces from name
     name = re.sub(' ,', ',', re.sub(' +', ' ', meal['name'].rstrip()))
+
     # format price with comma
     price = str(meal['prices'][args['price']]).replace('.',',') + " €"
+
     # build category string
-    categories = meal['category'].split()
+    categories   = meal['category'].split()
     category_str = ''
     for category in set(categories):
-        if category in category_map:
-            category_str += category_map[category]
+        if category in category_map: category_str += category_map[category]
     # add brackets to category string if not empty
-    if not category_str == '':
-        category_str = ' [' + category_str + '] '
-    else:
-        category_str = ' '
+    if not category_str == '': category_str = ' [' + category_str + '] '
+    else:                      category_str = ' '
 
     # print meal
     print(intent() + colorize(number + name + category_str + price, 'cyan'))
@@ -135,6 +141,8 @@ for meal in meals:
 
 print()
 
+###############################################################################
+# Finish-Line
 # print legend
 print(intent(), end='')
 for category in category_map:
